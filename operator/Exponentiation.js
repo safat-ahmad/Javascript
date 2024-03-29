64c53682690aa6349b72f030bb08e392ef283a95
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-03-29 09:56:02 +0530

    padEnd method

--- operator/Exponentiation.js
+++ operator/Exponentiation.js
@@ -4,5 +4,15 @@ let b = a ** 2;
 let x = 10;
 let z = x ** 3;
 
+let c = 10;
+let d = (c ** 2);
+
+let e = 5;
+e **= 3;
+
+
+
+console.log(e);
+console.log(d);
 console.log(z);
 console.log(b);
---
+++ padding.js
@@ -0,0 +1,16 @@
+let y = "5";
+y = y.padStart(4, "0");
+
+let x = "5";
+x = x.padEnd(5, "0");
+
+let z = "5";
+z = z.padEnd(6, "5");
+
+let a = "5";
+a = a.padStart(5,"e");
+
+console.log(a);
+console.log(z);
+console.log(y);
+console.log(x);
