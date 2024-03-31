ed053ee8c1de8f0d406796acf0c03ab856cdd72e
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-03-31 11:42:08 +0530

    arrays in javascript

---
+++ array/array1.js
@@ -0,0 +1,16 @@
+const fruits = ["mango", "banana","apple","grapes"];
+let length = fruits.length;
+console.log(length);
+
+const fruit = ["mango", "banana","apple","grapes","anannas"]; 
+console.log(fruit[fruit.length-1]);
+
+const cars = ["BMW","scorpio","mahindra","TaTa","Toyota" ];
+cars[3] ="Creta";
+console.log(cars);
+
+const car = ["BMW","scorpio","mahindra","TaTa","Toyota" ];
+car.push("Lenovo","Innova","Volvo");
+let totalcars = car.length;
+console.log(car);
+console.log(totalcars);
