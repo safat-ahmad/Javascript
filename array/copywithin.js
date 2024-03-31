7ac9bba120a58ec8214ce84dabcec283488bcbcb
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-04-01 00:44:53 +0530

    copywidth in array

---
+++ array/copywithin.js
@@ -0,0 +1,29 @@
+const fruits = ["Mango","Banana","anannaz","Orange","Apple","ABC","CD",""];
+// console.log(fruits.copyWithin(2,0));
+
+// const fruit = ["Mango","Banana","anannaz","Orange"];
+
+const fruitss = [["h"],
+["Mango","Banana",
+["anannaz","Apple"],
+"Orange"],
+"S"];
+// console.log(fruitss[1][2][1]);
+
+const arr = [
+    {
+        name: "Safat Ahmad",
+        age: 23
+    },
+    {
+        name:"Manisha Chaddi Chor",
+        age: 29,
+        fruits: ["apple","banana",["mango","melony"],["grape",{
+            name:"melony",
+            rishta: "any"
+        }]]
+    }
+]
+console.log(arr[1].fruits[3][1].name,"-->",arr[1].fruits[3][1].rishta);
+
+
