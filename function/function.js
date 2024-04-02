2b96c43ff60c901c1e66dbbc3dc1aa079970e31f
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-04-02 12:20:21 +0530

    javascript function

---
+++ function/function.js
@@ -0,0 +1,13 @@
+function myfunction(a, b)
+{
+    return a * b;
+}
+let x = myfunction(5, 3);
+console.log(x);
+
+function myfunctions(a, b, c)
+{
+    return a * b * c;
+}
+let y = myfunctions(10, 15, 20);
+console.log(y);
