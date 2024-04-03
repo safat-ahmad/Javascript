a986e670f0d9a3a6c0c661593214aa6c573d9b70
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-04-03 10:06:40 +0530

    practice basic javascript

---
+++ learn basic javascript/script1.js
@@ -0,0 +1,26 @@
+let a = "safat";
+let b = 5;
+console.log(a + b);
+console.log(typeof (a + b));
+
+//No changes any const var
+const c = {
+    name: "safat ahmad",
+    section: 1,
+    isprinciple: false
+}
+c["friend"] = "khan";
+c["name"] = "safat";
+console.log(c);
+
+const dict = {
+    appreciate: "to enjoy something or to understand the value of somebody/something",
+    game: "an amusement or pastime",
+    plan: "a design or scheme of arrangement",
+    suddenly: "quickly and without warning",
+    great: "A great fire destroyed nearly half the city"
+}
+console.log(dict.great);
+console.log(dict["game"],dict["great"]);
+
+
---
+++ learn basic javascript/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(dict["game"],dict["great"]);
