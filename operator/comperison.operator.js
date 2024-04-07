41c5f88ec8dcc793a4731e8f45b28a8f169bdfa8
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-04-07 10:27:27 +0530

    comperison operator in javascript

---
+++ operator/comperison.operator.js
@@ -0,0 +1,15 @@
+//conperison operator
+let a = 10;
+let b = 20;
+console.log("a == b is",a == b);
+console.log("a == b is",a != b);
+console.log("a > b is", a > b);
+console.log("a < b is", a < b);
+
+
+
+let x = 20;
+let y = "20";
+console.log("x === y is", x === y); 
+console.log("x !== y is", x !== y); 
+
