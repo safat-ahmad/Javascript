e8fe711aa0d8b6a85e790a13464a806910186a93
Safat Ahmad <safatahmadkhan65@gmail.com>
2024-04-15 08:35:36 +0530

    conditional statement

---
+++ practice javascript/script.js
@@ -0,0 +1,28 @@
+
+//if statement
+let userInput = 30;
+if (userInput >= 18) {
+  console.log("You are eligible to vote.");
+}
+
+// if else statement
+let password = "safat123456";
+if (password === "safat123456") {
+  console.log("access granted");
+}
+else{
+  console.log("try again");
+}
+
+//if else if  else statement
+let grade = 100;
+
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else {
+  console.log("D");
+}
