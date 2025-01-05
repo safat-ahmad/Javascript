a869d1dcf3c8acfcabbc848b20833f7e9ff679e0
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-05 15:27:08 +0530

    multidimentional array in javascript

---
+++ multidimentional-aaray.js
@@ -0,0 +1,13 @@
+var ary=[
+    ["Safat",18,"Male","BCA" ],
+    ["MMM",19,"Male","B.COM"],
+    ["KKKK",21,"Female","BA"]
+]
+for(var a=0; a<3; a++){
+    for(var b=0; b<4; b++){
+        console.log(ary[a][b])+ " ";
+
+    }
+    console.log();
+
+    }
